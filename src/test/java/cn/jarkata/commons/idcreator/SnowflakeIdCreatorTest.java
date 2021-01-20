b5b816ec15cdf22c5bcc70cd4b536ad4466a3563
vkata <fx.windy@foxmail.com>
2021-01-20 10:04:19 +0800

    优化ID生成

--- src/test/java/cn/jarkata/commons/idcreator/SnowflakeIdCreatorTest.java
+++ src/test/java/cn/jarkata/commons/idcreator/SnowflakeIdCreatorTest.java
@@ -41,9 +41,9 @@ public class SnowflakeIdCreatorTest {
         String id = IdFactory.createId(1, 27);
         System.out.println(id);
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 10000000; i++) {
-            String id2 = IdFactory.createId(1, 31);
-        }
+
+        String id2 = IdFactory.createId(1, 31);
+
         long dur = System.currentTimeMillis() - start;
 
         System.out.println(dur);
