49afaf2b5b454a116e1fc744ca9e274e0973e8f9
Jarkata <jarkata@foxmail.com>
2022-11-05 18:45:24 +0800

    字符串工具类

---
+++ src/main/java/cn/jarkata/commons/utils/StringUtils.java
@@ -0,0 +1,44 @@
+package cn.jarkata.commons.utils;
+
+import java.util.Objects;
+
+public final class StringUtils {
+
+    public static boolean isBlank(String str) {
+        return Objects.nonNull(str) && str.trim()
+                .length() <= 0;
+    }
+
+    public static boolean isNotBlank(String str) {
+        return !isBlank(str);
+    }
+
+    public static String defaultIfBlank(String str, String defaultVal) {
+        return isBlank(str) ? defaultVal : str;
+    }
+
+    public static String trimToEmpty(String str) {
+        return isBlank(str) ? "" : str.trim();
+    }
+
+    public static String trimToNull(String str) {
+        return isBlank(str) ? null : str.trim();
+    }
+
+    public static String trimJson(String str) {
+        if (isBlank(str)) {
+            return "";
+        }
+        str = str.replaceAll("\\\"\\{", "{");
+        str = str.replaceAll("\\}\\\"", "}");
+        str = str.replaceAll("\\\\", "");
+        str = str.replaceAll("\\\n", "");
+        str = str.replaceAll("\\\t", "");
+        str = str.replaceAll("\\\"\\[\\{", "[{");
+        str = str.replaceAll("\\}\\]\\\"", "}]");
+        return str;
+    }
+
+
+
+}
