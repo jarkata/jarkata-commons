156cd3ad280dcd55d233d84f3bd722c92ccdfb1d
Jarkata <jarkata@foxmail.com>
2022-11-05 19:02:17 +0800

    升级版本及增加测试类

---
+++ src/test/java/cn/jarkata/commons/utils/StringUtilsTest.java
@@ -0,0 +1,28 @@
+package cn.jarkata.commons.utils;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringUtilsTest {
+
+    @Test
+    public void testTrimJson() {
+        String json = StringUtils.trimJson("{\n" + "  \"test\": \"{\\\"test\\\":\\\"rwer\\\"}\"\n" + "}");
+        JSONObject jsonObject = JSON.parseObject(json);
+        JSONObject test = jsonObject.getJSONObject("test");
+        Assert.assertNotNull(test);
+    }
+
+
+    @Test
+    public void testTrimJsons() {
+        String json = StringUtils.trimJson("{\n" + "  \"test\": \"{\\\"test\\\":\\\"rwer\\\"}\",\n" + "  \"test2\": \"[{\\\"test\\\":\\\"rwer\\\"},{\\\"test\\\":\\\"rwer\\\"}]\"\n" + "}");
+        JSONObject jsonObject = JSON.parseObject(json);
+        Object test = jsonObject.get("test2");
+        Assert.assertTrue(test instanceof JSONArray);
+    }
+}
