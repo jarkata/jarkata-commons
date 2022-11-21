2c36d81833ee96fed4e5aacc9cf38afef528f45d
Jarkata <jarkata@foxmail.com>
2022-11-21 20:29:39 +0800

    增加读取文件内容及创建文件目录

--- src/main/java/cn/jarkata/commons/utils/FileUtils.java
+++ src/main/java/cn/jarkata/commons/utils/FileUtils.java
@@ -2,6 +2,7 @@ package cn.jarkata.commons.utils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -10,6 +11,40 @@ public class FileUtils {
 
     public static InputStream toInputStream(String str) {
         return new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static void write(File file, String message) throws IOException {
+        boolean directory = ensureDirectory(file);
+        if (!directory) {
+            throw new IllegalArgumentException(file + " Not Exist");
+        }
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(message.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+    public List<String> readFile(File file) throws IOException {
+        Objects.requireNonNull(file);
+        return readLines(Files.newInputStream(file.toPath()));
+    }
+
+    public static boolean ensureDirectory(File file) {
+        if (Objects.isNull(file)) {
+            return false;
+        }
+        if (file.isFile()) {
+            File parentFile = file.getParentFile();
+            if (!parentFile.exists()) {
+                return parentFile.mkdirs();
+            }
+        }
+        if (file.isDirectory()) {
+            if (file.exists()) {
+                return true;
+            }
+            return file.mkdirs();
+        }
+        return false;
     }
 
     public static InputStream getStream(String fileName) {
