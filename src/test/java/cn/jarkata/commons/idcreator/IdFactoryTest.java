d93b22b3e33ed6a3d3066adbf0c1dd81c7f2d508
Jarkata <jarkata@foxmail.com>
2023-08-14 22:04:13 +0800

    fix:date utils

--- src/test/java/cn/jarkata/commons/idcreator/IdFactoryTest.java
+++ src/test/java/cn/jarkata/commons/idcreator/IdFactoryTest.java
@@ -1,10 +1,17 @@
 package cn.jarkata.commons.idcreator;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class IdFactoryTest {
 
     @Test
     public void createId() {
+        long id = IdFactory.createId(1);
+        Assert.assertTrue(id > 0);
+    }
+
+    @Test
+    public void testCreateId() {
     }
 }
